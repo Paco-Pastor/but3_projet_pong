fa2689e5189256e1565a592e51ef5a4aac7bf3c1
Kévin Grévin <ke.grevin@gmail.com>
2023-12-15 17:22:16 +0100

    camel case to snake case

--- src/images.py
+++ src/images.py
@@ -6,13 +6,13 @@ class PaddleSprite:
         self.default = None
         self.large = None
 
-    def setDefault(self, path):
+    def set_default(self, path):
         self.default = pygame.image.load(path)
 
-    def setLarge(self, path):
+    def set_large(self, path):
         self.large = pygame.image.load(path)
 
 
 DEFAULT = PaddleSprite()
-DEFAULT.setDefault("./img/default_paddle.png")
-DEFAULT.setLarge("./img/large_paddle.png")
+DEFAULT.set_default("./img/default_paddle.png")
+DEFAULT.set_large("./img/large_paddle.png")
