10c9ccd941b9a63cab9700f9c08c78ef4dba2f0b
Kévin Grévin <ke.grevin@gmail.com>
2024-03-19 18:41:25 +0100

    Event handling

---
+++ src/event.py
@@ -0,0 +1,20 @@
+from tkinter import EventType
+
+
+class Event:
+    # extra : true if it's on the left side, false if it's on the right side
+    BALL_OUT = 'Ball Out'
+    def __init__(self, event_type, extra=None):
+        ok = False
+        if event_type is Event.BALL_OUT:
+            if type(extra) == bool:
+                self.type = event_type
+                self.extra = extra
+                ok = True
+
+        if not ok:
+            raise TypeError('Invalid input')
+
+
+    def __repr__(self):
+        return f'{self.type}: {self.extra}'
