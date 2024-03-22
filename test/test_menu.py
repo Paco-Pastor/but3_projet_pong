5595fbf7c51512019f5cd73b779761e5135d8fd3
Paco-Pastor <paco.pastor@hippolyte-rh.fr>
2024-03-22 13:29:53 +0100

    ✅ Fix start_menu tests

--- test/test_menu.py
+++ test/test_menu.py
@@ -7,14 +7,19 @@ class TestMenu(TestCase):
     @mock.patch("pygame.display")
     @mock.patch("pygame_menu.Menu")
     def test_start_menu(self, mock_pygame_menu, mock_display):
-        menu.start_menu(100, 100)
+        mock_width = 100
+        mock_height = 100
+        menu.start_menu(mock_width, mock_height)
         expected_calls = [
-            mock.call().add.selector("Mode :", [("1 joueur", 1), ("2 joueurs", 2)]),
-            mock.call().add.button('Jouer', mock.ANY),
-            mock.call().add.button('Quitter', mock.ANY),
-            mock.call().mainloop(mock.ANY)
+            mock.call("Pong eternal", mock_width, mock_height, theme=mock.ANY),
+            mock.call().add.selector("Mode :", mock.ANY),
+            mock.call().add.selector("Score maximal :", mock.ANY, onchange=mock.ANY),
+            mock.call().add.selector("Graphisme :", mock.ANY, default=1),
+            mock.call().add.button("Jouer", mock.ANY),
+            mock.call().add.button("Quitter", mock.ANY),
+            mock.call().mainloop(mock.ANY),
         ]
-        self.assertIn(expected_calls, mock_pygame_menu.mock_calls)
+        self.assertEqual(expected_calls, mock_pygame_menu.mock_calls)
 
     def test_menu_too_small(self):
         with self.assertRaises(ValueError):
