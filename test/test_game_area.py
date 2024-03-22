9c29a72abd50401cb817628e50203351438bb7fb
Paco-Pastor <paco.pastor@hippolyte-rh.fr>
2024-03-22 11:29:56 +0100

    ✅ Edit test game > test game area

---
+++ test/test_game_area.py
@@ -0,0 +1,12 @@
+from unittest import TestCase, mock, main
+
+
+class TestGameArea(TestCase):
+
+    # TODO
+    def test_execute_once(self):
+        pass
+
+
+if __name__ == "__main__":
+    main()
